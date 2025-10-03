619f1c3469c6d9731c29a0e46b4086835653407e
Davi Aguilar <129971903+DaviAguilar@users.noreply.github.com>
2025-10-02 23:03:14 -0300

    Add function to calculate analysis time in hours

---
+++ Lab03/scripts/utils.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+def calcular_tempo_analise(criado_em, fechado_em):
+    """Calcula tempo de anÃ¡lise do PR em horas."""
+    if not criado_em or not fechado_em:
+        return None
+    formato = "%Y-%m-%dT%H:%M:%SZ"
+    dt_criado = datetime.strptime(criado_em, formato)
+    dt_fechado = datetime.strptime(fechado_em, formato)
+    diff = dt_fechado - dt_criado
+    return diff.total_seconds() / 3600
