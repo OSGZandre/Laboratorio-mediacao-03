021eae476fbf5fbd6463f86483056755440394a8
OSGZandre <andrealmeidaaz105@gmail.com>
2025-10-09 22:55:16 -0300

    arrumando untils

--- Lab03/scripts/utils.py
+++ Lab03/scripts/utils.py
@@ -1,11 +1,7 @@
 from datetime import datetime
 
 def calcular_tempo_analise(criado_em, fechado_em):
-<<<<<<< HEAD
-    """Calcula tempo de análise do PR em horas."""
-=======
     """Calcula tempo de anÃ¡lise do PR em horas."""
->>>>>>> 619f1c3469c6d9731c29a0e46b4086835653407e
     if not criado_em or not fechado_em:
         return None
     formato = "%Y-%m-%dT%H:%M:%SZ"
